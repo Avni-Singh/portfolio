1b674d4d94d196249f785f918d39a1120cadf9c5
Avni Singh <avnisingh@wpswings.com>
2025-06-19 14:33:26 +0530

    add jquery for profile img

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 $(document).ready(function () {
   // ✅ Hover effect on each social stat card
-  $('.web-main_sec-socials-art-card li').hover(
+  $(".web-main_sec-socials-art-card li").hover(
     function () {
       $(this).css({
-        transform: 'scale(1.05)',
-        boxShadow: '0 4px 20px rgba(0, 0, 0, 0.2)'
+        transform: "scale(1.05)",
+        boxShadow: "0 4px 20px rgba(0, 0, 0, 0.2)",
       });
     },
     function () {
       $(this).css({
-        transform: 'scale(1)',
-        boxShadow: 'none'
+        transition: "all 0.3s ease",
+        transform: "scale(1)",
+        boxShadow: "none",
+      });
+    }
+  );
+
+  // image
+
+  $(".web-main_s-art.web-main_s-art-banner-image").hover(
+    function () {
+      $(this).css({
+        transform: "scale(1.05)",
+      });
+    },
+    function () {
+      $(this).css({
+        transition: "all 0.3s ease",
+        transform: "scale(1)",
       });
     }
   );
 
   // ✅ Click alert on Subscribe button
-  $('button').click(function () {
-    alert('Thanks for subscribing!');
+  $("button").click(function () {
+    alert("Thanks for subscribing!");
   });
 });
