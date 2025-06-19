7a0851936bf3c17af62bf59df296bed4e0424d52
Avni Singh <avnisingh@wpswings.com>
2025-06-19 11:09:31 +0530

    add file and link of js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
       $(document).ready(function () {
         $('button').click(function () {
-          alert('Thanks for subscribing!');
+           $(this).hide();
         });
       });
    
