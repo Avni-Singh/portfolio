b01daab47741f94f599f19d2c40557e29a90e67a
Avni Singh <avnisingh@wpswings.com>
2025-06-19 12:23:06 +0530

    add slert msg using jquery on button

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-      $(document).ready(function () {
-        $('button').click(function () {
-           $(this).hide();
-        });
-      });
-   
+$(document).ready(function () {
+  $("button").click(function () {
+    alert("Thanks for subscribing!");
+  });
+});
